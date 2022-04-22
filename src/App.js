ad04b9029fd5c6e8df8513d8df3cd6896712a6d4
Linha10 <yahaa1010@gmail.com>
2022-04-22 12:38:13 +0800

    Use Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
+import { act } from 'react-dom/test-utils';
 import { useSelector, useDispatch } from 'react-redux';
 import './counter.css'
+import {actions} from './store/index' //import redux toolkit setting
 
 function App() {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
   const incre = () => {
-    dispatch({ type: 'INC' });
+    dispatch (actions.incre()); ////Using redux toolkit -- fisrt line
+    // dispatch({ type: 'INC' }); not using toolkit
   }
 
   const decre = () => {
-    dispatch({ type: 'DEC' });
+    dispatch ( actions.decre());
+    // dispatch({ type: 'DEC' });
   }
 
   const addBy = () => {
-    dispatch({ type: 'ADD', payload: 10 })
+    dispatch (actions.addBy(20))
+    // dispatch({ type: 'ADD', payload: 10 })
   }
   const tenTimes = () => {
-    dispatch({ type: 'TENTIMES', payload:10 })
+    dispatch (actions.tenTImes(10))
+    // dispatch({ type: 'TENTIMES', payload:10 })
   }
   const RESET = () => {
-    dispatch ({type:'RESTNUM'})
+    dispatch (actions.RESET(0))
+  //   // dispatch ({type:'RESTNUM'})
   }
   return (
     <div className='styling'>
@@ -27,7 +34,7 @@ function App() {
       <h2>{counter}</h2>
       <button onClick={incre}>+1</button>
       <button onClick={decre}>-1</button>
-      <button onClick={addBy}>+10</button>
+      <button onClick={addBy}>+20</button>
       <button onClick={tenTimes}>X10 </button>
       <button onClick={RESET}>RESET</button>
     </div>
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,28 +1,59 @@
 import { createStore } from 'redux';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-const reducerFunc = (state = { counter: 0 }, action) => {
-    
-
-    if (action.type === 'INC') {
-        return { counter: state.counter + 1 }
+//Using redux toolkit
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState : { counter : 0},
+    reducers: {
+        incre(state, action) { 
+            state.counter ++
+        },
+        decre(state, action) { 
+            state.counter --
+        },
+        addBy(state, action) {
+            state.counter += action.payload
+         },
+        tenTImes(state, action) { 
+            state.counter *= action.payload
+        },
+        RESET(state, action) { 
+            state.counter *= action.payload
+        },
     }
+})
+export const actions = counterSlice.actions
 
-    if (action.type === 'DEC') {
-        return { counter: state.counter - 1 }
-    }
-    if (action.type === 'ADD') {
-        return { counter: state.counter + action.payload }
-    }
-    if (action.type === 'TENTIMES') {
-        return { counter: state.counter * action.payload }
-    }
-    if (action.type === 'RESTNUM') {
-        return { counter: state.counter = 0}
-    }
-    return state;
-}
-
-const store = createStore(reducerFunc);
-
-
+const store = configureStore({
+    reducer : counterSlice.reducer
+})
 export default store;
+
+//----[not using the redux toolkit down below---
+
+// const reducerFunc = (state = { counter: 0 }, action) => {
+
+//     if (action.type === 'INC') {
+//         return { counter: state.counter + 1 }
+//     }
+
+//     if (action.type === 'DEC') {
+//         return { counter: state.counter - 1 }
+//     }
+//     if (action.type === 'ADD') {
+//         return { counter: state.counter + action.payload }
+//     }
+//     if (action.type === 'TENTIMES') {
+//         return { counter: state.counter * action.payload }
+//     }
+//     if (action.type === 'RESTNUM') {
+//         return { counter: state.counter = 0}
+//     }
+//     return state;
+// }
+
+// const store = createStore(reducerFunc);
+
+
+// export default store;
